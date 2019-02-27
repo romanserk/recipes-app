f565f310450e1e05782e48e00ebd95dd23f5e7f9
romanserk <romanserk@gmail.com>
2019-02-27 20:11:44 +0200

    creat and test API call

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
-// Global app controller
-import num from './test';
+import axios from 'axios';
 
-console.log(`i imported ${num} from enother module called test.js! Variable x is ${x}`);
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/'
+    const key = 'cb83c520e8a0a51cbce156c79205a1b7';
+    try {
+        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+    
+}
+getResults('tomato pasta');
+
