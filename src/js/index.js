6f96459864e220bd8c4ad26343d49624c333b4f5
romanserk <romanserk@gmail.com>
2019-02-28 15:36:50 +0200

    build search view

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,38 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView'
+import { elements } from './views/base';
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+const controlSearch = async () => {
+
+    // 1. get query from view
+    const query = searchView.getInput();
+
+    if (query) {
+        // 2. new search object and add to state
+        state.search = new Search(query);
+
+        // 3. preapere UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        // 4. search for recipes
+        await state.search.getResults();
+
+        // 5. render results on UI
+        searchView.renderResults(state.search.result);
+    }
+
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -10,7 +10,7 @@ export default class Search {
         // recive API without domain
         const proxy = 'https://cors-anywhere.herokuapp.com/'
         // the API key from www.food2fork.com
-        const key = 'cb83c520e8a0a51cbce156c79205a1b7';
+        const key = 'aed7b1f043f8cac58eee9ef336b9f740';
         try {
             const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
