0c5e3dedb7fe5976e5c25b3f49b14f4f8866340e
romanserk <romanserk@gmail.com>
2019-02-28 16:23:27 +0200

    limit the recipes titles

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -12,6 +12,35 @@ export const clearResults = () => {
     elements.searchResList.innerHTML = '';
 };
 
+/* exmple title : 
+ * 'pasta with tomato and spinach'
+ * 
+ * reduce iteration example:
+ * acc:0 / cur = pasta / curr.length = 5 / acc + curr.length = 5
+ * newTitle = ['pasta']
+ * acc:5 / cur = with / curr.length = 4 / acc + curr.length = 9
+ * newTitle = ['pasta','with']
+ * acc:9 / cur = tomato / curr.length = 6 / acc + curr.length = 15
+ * newTitle = ['pasta','with','tomato']
+ * acc:15 / cur = and / curr.length = 3 / acc + curr.length = 18
+ * 18 is > then limit = 17 so next words do not includs into the newTitle
+ */
+
+const limitRecipeTile = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+        //return the result
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+};
+
 const renderRecipe = recipe => {
     const markup = `
     <li>
@@ -20,7 +49,7 @@ const renderRecipe = recipe => {
                 <img src="${recipe.image_url}" alt="${recipe.title}">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTile(recipe.title)}</h4>
                 <p class="results__author">${recipe.publisher}</p>
             </div>
         </a>
