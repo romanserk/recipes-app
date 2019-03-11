d9fc57011d8b74120a0cab5d8b9d1d4be46f01bd
romanserk <romanserk@gmail.com>
2019-03-12 00:26:27 +0200

    shopping list model

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView'
 import * as recipeView from './views/recipeView'
 import {
